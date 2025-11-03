36e089ed50eb8648869baeef984985c92322c793
Kyle Pollard <kylejrp@gmail.com>
2025-11-02 22:03:22 -0800

    Fix benchmark

--- Forklift.Benchmark/Program.cs
+++ Forklift.Benchmark/Program.cs
@@ -2,7 +2,7 @@
 
 Console.WriteLine("Perft test starting...");
 var b = BoardFactory.FromFenOrStart("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1");
-b.KeepTrackOfRepetitions = false;
+b.KeepTrackOfHistory = false;
 var stats = Perft.Statistics(b, 5);
 Console.WriteLine(stats.Nodes);
 Console.WriteLine("Perft test completed.");
