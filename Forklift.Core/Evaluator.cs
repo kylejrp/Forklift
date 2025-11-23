34883212540d211971f603775377c3d73e3b2223
Kyle Pollard <kylejrp@gmail.com>
2025-11-23 01:52:32 -0800

    Switch to PeSTO evaluation

--- Forklift.Core/Evaluator.cs
+++ Forklift.Core/Evaluator.cs
@@ -1,87 +1,289 @@
 using System;
 using System.Diagnostics;
 using System.Numerics;
-using Forklift.Core;
+using System.Runtime.CompilerServices;
 
 namespace Forklift.Core
 {
     public static class Evaluator
     {
-        private static readonly int[] TypeValues = {
-            100, // Pawn
-            320, // Knight
-            330, // Bishop
-            500, // Rook
-            900, // Queen
-            0    // King
+        // PeSTO material values
+        private static readonly int[] MgValue = { 82, 337, 365, 477, 1025, 0 };
+        private static readonly int[] EgValue = { 94, 281, 297, 512, 936, 0 };
+
+        public const int MaxEvaluationScore = 30_000;
+        public const int MinEvaluationScore = -MaxEvaluationScore;
+        private const int PhaseMax = 24;
+
+        // Piece-square tables from PeSTO (Rofchade)
+        private static readonly int[][] MgPestoTable =
+        {
+            // Pawn
+            new[]
+            {
+                  0,   0,   0,   0,   0,   0,  0,   0,
+                 98, 134,  61,  95,  68, 126, 34, -11,
+                 -6,   7,  26,  31,  65,  56, 25, -20,
+                -14,  13,   6,  21,  23,  12, 17, -23,
+                -27,  -2,  -5,  12,  17,   6, 10, -25,
+                -26,  -4,  -4, -10,   3,   3, 33, -12,
+                -35,  -1, -20, -23, -15,  24, 38, -22,
+                  0,   0,   0,   0,   0,   0,  0,   0,
+            },
+            // Knight
+            new[]
+            {
+                -167, -89, -34, -49,  61, -97, -15, -107,
+                 -73, -41,  72,  36,  23,  62,   7,  -17,
+                 -47,  60,  37,  65,  84, 129,  73,   44,
+                  -9,  17,  19,  53,  37,  69,  18,   22,
+                 -13,   4,  16,  13,  28,  19,  21,   -8,
+                 -23,  -9,  12,  10,  19,  17,  25,  -16,
+                 -29, -53, -12,  -3,  -1,  18, -14,  -19,
+                -105, -21, -58, -33, -17, -28, -19,  -23,
+            },
+            // Bishop
+            new[]
+            {
+                -29,   4, -82, -37, -25, -42,   7,  -8,
+                -26,  16, -18, -13,  30,  59,  18, -47,
+                -16,  37,  43,  40,  35,  50,  37,  -2,
+                 -4,   5,  19,  50,  37,  37,   7,  -2,
+                 -6,  13,  13,  26,  34,  12,  10,   4,
+                  0,  15,  15,  15,  14,  27,  18,  10,
+                  4,  15,  16,   0,   7,  21,  33,   1,
+                -33,  -3, -14, -21, -13, -12, -39, -21,
+            },
+            // Rook
+            new[]
+            {
+                 32,  42,  32,  51, 63,  9,  31,  43,
+                 27,  32,  58,  62, 80, 67,  26,  44,
+                 -5,  19,  26,  36, 17, 45,  61,  16,
+                -24, -11,   7,  26, 24, 35,  -8, -20,
+                -36, -26, -12,  -1,  9, -7,   6, -23,
+                -45, -25, -16, -17,  3,  0,  -5, -33,
+                -44, -16, -20,  -9, -1, 11,  -6, -71,
+                -19, -13,   1,  17, 16,  7, -37, -26,
+            },
+            // Queen
+            new[]
+            {
+                -28,   0,  29,  12,  59,  44,  43,  45,
+                -24, -39,  -5,   1, -16,  57,  28,  54,
+                -13, -17,   7,   8,  29,  56,  47,  57,
+                -27, -27, -16, -16,  -1,  17,  -2,   1,
+                 -9, -26,  -9, -10,  -2,  -4,   3,  -3,
+                -14,   2, -11,  -2,  -5,   2,  14,   5,
+                -35,  -8,  11,   2,   8,  15,  -3,   1,
+                 -1, -18,  -9,  10, -15, -25, -31, -50,
+            },
+            // King (middlegame)
+            new[]
+            {
+                -65,  23,  16, -15, -56, -34,   2,  13,
+                 29,  -1, -20,  -7,  -8,  -4, -38, -29,
+                 -9,  24,   2, -16, -20,   6,  22, -22,
+                -17, -20, -12, -27, -30, -25, -14, -36,
+                -49,  -1, -27, -39, -46, -44, -33, -51,
+                -14, -14, -22, -46, -44, -30, -15, -27,
+                  1,   7,  -8, -64, -43, -16,   9,   8,
+                -15,  36,  12, -54,   8, -28,  24,  14,
+            }
         };
 
-        public const int MaxEvaluationScore = 30_000; // Arbitrary large value representing a decisive advantage
+        private static readonly int[][] EgPestoTable =
+        {
+            // Pawn
+            new[]
+            {
+                  0,   0,   0,   0,   0,   0,   0,   0,
+                178, 173, 158, 134, 147, 132, 165, 187,
+                 94, 100,  85,  67,  56,  53,  82,  84,
+                 32,  24,  13,   5,  -2,   4,  17,  17,
+                 13,   9,  -3,  -7,  -7,  -8,   3,  -1,
+                  4,   7,  -6,   1,   0,  -5,  -1,  -8,
+                 13,   8,   8,  10,  13,   0,   2,  -7,
+                  0,   0,   0,   0,   0,   0,   0,   0,
+            },
+            // Knight
+            new[]
+            {
+                -58, -38, -13, -28, -31, -27, -63, -99,
+                -25,  -8, -25,  -2,  -9, -25, -24, -52,
+                -24, -20,  10,   9,  -1,  -9, -19, -41,
+                -17,   3,  22,  22,  22,  11,   8, -18,
+                -18,  -6,  16,  25,  16,  17,   4, -18,
+                -23,  -3,  -1,  15,  10,  -3, -20, -22,
+                -42, -20, -10,  -5,  -2, -20, -23, -44,
+                -29, -51, -23, -15, -22, -18, -50, -64,
+            },
+            // Bishop
+            new[]
+            {
+                -14, -21, -11,  -8, -7,  -9, -17, -24,
+                 -8,  -4,   7, -12, -3, -13,  -4, -14,
+                  2,  -8,   0,  -1, -2,   6,   0,   4,
+                 -3,   9,  12,   9, 14,  10,   3,   2,
+                 -6,   3,  13,  19,  7,  10,  -3,  -9,
+                -12,  -3,   8,  10, 13,   3,  -7, -15,
+                -14, -18,  -7,  -1,  4,  -9, -15, -27,
+                -23,  -9, -23,  -5, -9, -16,  -5, -17,
+            },
+            // Rook
+            new[]
+            {
+                13, 10, 18, 15, 12,  12,   8,   5,
+                11, 13, 13, 11, -3,   3,   8,   3,
+                 7,  7,  7,  5,  4,  -3,  -5,  -3,
+                 4,  3, 13,  1,  2,   1,  -1,   2,
+                 3,  5,  8,  4, -5,  -6,  -8, -11,
+                -4,  0, -5, -1, -7, -12,  -8, -16,
+                -6, -6,  0,  2, -9,  -9, -11,  -3,
+                -9,  2,  3, -1, -5, -13,   4, -20,
+            },
+            // Queen
+            new[]
+            {
+                 -9,  22,  22,  27,  27,  19,  10,  20,
+                -17,  20,  32,  41,  58,  25,  30,   0,
+                -20,   6,   9,  49,  47,  35,  19,   9,
+                  3,  22,  24,  45,  57,  40,  57,  36,
+                -18,  28,  19,  47,  31,  34,  39,  23,
+                -16, -27,  15,   6,   9,  17,  10,   5,
+                -22, -23, -30, -16, -16, -23, -36, -32,
+                -33, -28, -22, -43,  -5, -32, -20, -41,
+            },
+            // King (endgame)
+            new[]
+            {
+                -74, -35, -18, -18, -11,  15,   4, -17,
+                -12,  17,  14,  17,  17,  38,  23,  11,
+                 10,  17,  23,  15,  20,  45,  44,  13,
+                 -8,  22,  24,  27,  26,  33,  26,   3,
+                -18,  -4,  21,  24,  27,  23,   9, -11,
+                -19,  -3,  11,  21,  23,  16,   7,  -9,
+                -27, -11,   4,  13,  14,   4,  -5, -17,
+                -53, -34, -21, -11, -28, -14, -24, -43
+            }
+        };
+
+        // Precomputed MG/EG tables indexed by PieceIndex (0..11) and 0x64 square (0..63).
+        private static readonly int[,] MgTable = new int[12, 64];
+        private static readonly int[,] EgTable = new int[12, 64];
+
+        static Evaluator()
+        {
+            // PieceIndex: 0..5 = white Pawn..King, 6..11 = black Pawn..King
+            for (int pieceIndex = 0; pieceIndex < 12; pieceIndex++)
+            {
+                bool isWhite = pieceIndex < 6;
+                int typeIndex = pieceIndex % 6; // 0..5 = Pawn..King
+
+                for (int sq = 0; sq < 64; sq++)
+                {
+                    // The PeSTO arrays are defined Top-Down (Rank 8 to Rank 1).
+                    // Our board squares are Bottom-Up (Rank 1 to Rank 8).
+
+                    int tableIndex;
+
+                    if (isWhite)
+                    {
+                        // White is normal, but needs to flip the array definition
+                        // to match the board square index.
+                        // Board A1 (0) -> Array Index 56 (Last Row)
+                        tableIndex = sq ^ 56;
+                    }
+                    else
+                    {
+                        // Black needs to be mirrored relative to White.
+                        // Board A8 (56) is Black's "Rank 1".
+                        // This corresponds to Array Index 56 (Last Row).
+                        // Board A1 (0) is Black's "Rank 8".
+                        // This corresponds to Array Index 0 (First Row).
+
+                        // Therefore, for Black, the board index matches the array index
+                        // due to the double negative (Flipped Board vs Flipped Array).
+                        tableIndex = sq;
+                    }
+
+                    MgTable[pieceIndex, sq] = MgValue[typeIndex] + MgPestoTable[typeIndex][tableIndex];
+                    EgTable[pieceIndex, sq] = EgValue[typeIndex] + EgPestoTable[typeIndex][tableIndex];
+                }
+            }
+        }
 
         /// <summary>
-        /// Static evaluation of the board position.
+        /// Static evaluation from White's point of view (PeSTO tapered eval).
+        /// Positive is good for White, negative is good for Black.
         /// </summary>
-        /// <param name="board">The board to evaluate.</param>
-        /// <returns>The static evaluation score of the board position, positive if favorable to White, negative if favorable to Black. Will always be between -<see cref="MaxEvaluationScore"/> and <see cref="MaxEvaluationScore"/>.</returns>
         public static int StaticEvaluate(Board board)
         {
-            int score = 0;
+            int mgWhite = 0, mgBlack = 0;
+            int egWhite = 0, egBlack = 0;
+            int gamePhase = 0;
+
             foreach (var piece in Piece.AllPieces)
             {
                 var pieceBitboard = board.GetPieceBitboard(piece);
-                score += BitOperations.PopCount(pieceBitboard) * TypeValues[piece.TypeIndex] * (piece.IsWhite ? 1 : -1);
-                score += EvaluatePieceTypePositional(piece, pieceBitboard);
+                var isWhite = piece.IsWhite;
+                while (pieceBitboard != 0)
+                {
+                    UnsafeSquare0x64 sq64 = (UnsafeSquare0x64)BitOperations.TrailingZeroCount(pieceBitboard);
+                    pieceBitboard &= pieceBitboard - 1;
+                    int pieceIndex = piece.PieceIndex; // 0..11
+
+                    if (isWhite)
+                    {
+                        mgWhite += MgTable[pieceIndex, sq64];
+                        egWhite += EgTable[pieceIndex, sq64];
+                    }
+                    else
+                    {
+                        mgBlack += MgTable[pieceIndex, sq64];
+                        egBlack += EgTable[pieceIndex, sq64];
+                    }
+
+                    gamePhase += GetGamePhaseIncrement(piece.Type);
+                }
             }
 
-            Debug.Assert(score >= -MaxEvaluationScore && score <= MaxEvaluationScore, $"Evaluation score of {score} out of bounds of ±{MaxEvaluationScore} for position {board.GetFEN()}.");
+            // Tapered eval
+            int mgScore = mgWhite - mgBlack;
+            int egScore = egWhite - egBlack;
+
+            int mgPhase = gamePhase;
+            if (mgPhase > PhaseMax) mgPhase = PhaseMax; // promotions, etc.
+            int egPhase = PhaseMax - mgPhase;
+
+            int score = (mgScore * mgPhase + egScore * egPhase) / PhaseMax;
+
+            Debug.Assert(score >= MinEvaluationScore && score <= MaxEvaluationScore,
+                $"Evaluation score of {score} out of bounds of ±{MaxEvaluationScore} for position {board.GetFEN()}.");
+
 
             return score;
         }
 
-        private static int EvaluatePieceTypePositional(Piece piece, ulong pieceBitboard)
-        {
-            return piece.Type switch
-            {
-                Piece.PieceType.Pawn => EvaluatePawnPositions(pieceBitboard, piece.Color),
-                _ => 0
-            };
-        }
-
-        private static int EvaluatePawnPositions(ulong pawnBitboard, Color color)
-        {
-            const int bonus = 10;
-            int score = 0;
-
-            while (pawnBitboard != 0)
-            {
-                UnsafeSquare0x64 sq64 = (UnsafeSquare0x64)BitOperations.TrailingZeroCount(pawnBitboard);
-                pawnBitboard &= pawnBitboard - 1; // Remove the least significant bit
-
-                int file = sq64.File;
-                int rank = sq64.Rank;
-
-                if (color == Color.Black)
-                {
-                    rank = 7 - rank; // Mirror rank for black pawns
-                }
-
-                if ((file == 3 || file == 4) && (rank == 3 || rank == 4))
-                {
-                    score += bonus;
-                }
-            }
-            return score * (color == Color.White ? 1 : -1);
-        }
-
         /// <summary>
-        /// Evaluates the board position from the perspective of the side to move.
+        /// Evaluates the board position from the perspective of the side to move
+        /// using PeSTO's tapered evaluation.
         /// </summary>
-        /// <param name="board">The board to evaluate.</param>
-        /// <returns>The evaluation score from the perspective of the side to move, positive if favorable to the side to move, negative if unfavorable. Will always be between -<see cref="MaxEvaluationScore"/> and <see cref="MaxEvaluationScore"/>.</returns>
         public static int EvaluateForSideToMove(Board board)
         {
-            int staticScore = StaticEvaluate(board);
-            return board.SideToMove == Color.White ? staticScore : -staticScore;
+            int whitePerspective = StaticEvaluate(board);
+            return board.SideToMove == Color.White ? whitePerspective : -whitePerspective;
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int GetGamePhaseIncrement(Piece.PieceType type) =>
+            type switch
+            {
+                Piece.PieceType.Knight => 1,
+                Piece.PieceType.Bishop => 1,
+                Piece.PieceType.Rook => 2,
+                Piece.PieceType.Queen => 4,
+                _ => 0 // Pawns and Kings do not affect game phase in PeSTO
+            };
     }
 }
